c84fa6d53859dbeef5522c5c90b36eeea8fdabff
VISHWANATH-1 <vishwanathpandey11@gmail.com>
2023-03-18 16:21:31 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Sidebar from './component/Sidebar/Sidebar';
+import MainDash from './component/MainDash/MainDash'
+import RightSide from './component/RightSide/RightSide';
+function App() {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar />
+        <MainDash />
+        <RightSide />
+      </div>
+    </div>
+  );
+}
+
+export default App;
